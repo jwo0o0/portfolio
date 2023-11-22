749dbb2a835a9a65e05bf5ca415d37e051626a3f
jwo0o0 <jwo0o0358@gmail.com>
2023-11-22 22:43:18 +0900

    add meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import { Info } from "@/components/Info/Info";
 import { Projects } from "@/components/Projects/Projects";
 import { Experiences } from "@/components/Experiences/Experiences";
@@ -7,6 +9,14 @@ import { ScrollToTop } from "@/components/common/ScrollToTop";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>개발자 김정우 ^O^</title>
+        <meta
+          name="description"
+          content="개발자 김정우를 소개합니다 ٩(๑❛ᴗ❛๑)۶"
+        ></meta>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Info />
       <Projects />
       <Experiences />
