c7cc4a6265a5a4190594fc74455968ecb798fd9f
jwo0o0 <jwo0o0358@gmail.com>
2024-12-30 20:39:32 +0900

    chore: remove existing files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID,
-  },
-};
-
-module.exports = nextConfig;
--- src/lib/gtag.js
+++
@@ -1,7 +0,0 @@
-const changeRouteGtag = (url) => {
-  window.gtag("config", process.env.NEXT_PUBLIC_GA_ID, {
-    page_path: url,
-  });
-};
-
-export default changeRouteGtag;
